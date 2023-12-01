75b0ce081175a2bbd8c7df7785348454ef3d01f2
Your Name <you@example.com>
2023-12-01 15:23:20 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload anf check.
+          Edit <code>src/App.js</code> and save to reload and check.
         </p>
         <a
           className="App-link"
